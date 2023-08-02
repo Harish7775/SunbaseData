82c21caf6d44f99053a385d131efa5ec85790ba1
Harish7775 <103637134+Harish7775@users.noreply.github.com>
2023-08-02 11:54:03 +0530

    add assignment sunbasedata

---
+++ SunbaseData/EmployeeRegistrationSystem/src/main/java/com/SunbaseData/Exception/EmployeeException.java
@@ -0,0 +1,11 @@
+package com.SunbaseData.Exception;
+
+public class EmployeeException extends Exception{
+    public EmployeeException(){
+
+
+    }
+    public EmployeeException(String msg){
+        super(msg);
+    }
+}
